bd9555a04184f1cbd37f12f3df668a707e4a04d7
Paul Thiel <lordnoname@t-online.de>
2016-01-24 19:39:14 +0100

    feat(): finish formlevel interface and add pair class
    update(): remove todo sign
    
    update(): remove toString method and add better description
    
    update(): update docs

--- src/core/interfaces/FormLevel.ts
+++ src/core/interfaces/FormLevel.ts
@@ -1,7 +1,10 @@
-﻿export class FormLevel {
+﻿import {Pair} from "../util/Pair";
+import {Subject} from "./Subject";
+
+export class FormLevel {
 
   level: number;
-  //TODO(lordnoname): add subject and weekly amount of this subject in a array
+  subjects: Array<Pair<Subject, number>>;
 
   /**
    * A grade level containing the weekly school subjects and hours
@@ -9,5 +12,35 @@
    */
   constructor(level: number) {
     this.level = level;
+    this.subjects = new Array<Pair<Subject, number>>();
+  }
+
+  /**
+   * Adds a weekly subject with their amount of hours per week
+   * @param subject Weekly subject of this form level
+   * @param amount Weekly amount of this subject
+   */
+  addSubject(subject: Subject, amount: number): void {
+    var pair: Pair<Subject, number> = new Pair<Subject, number>(subject, amount);
+    if (this.subjects.indexOf(pair) > -1) {
+      this.subjects.push(pair);
+    }
+  }
+
+  /**
+   * Removes a weekly subject with their amount of hours per week
+   * @param pair The pair of the subject which should be removed from this form level
+   */
+  removeSubject(subject: Subject): void {
+    for (var pair in this.subjects) {
+      if (pair.first !== subject) {
+        continue;
+      }
+      var index: number = this.subjects.indexOf(pair, 0);
+      if (index != undefined) {
+        this.subjects.splice(index, 1);
+        return;
+      }
+    }
   }
 }
---
+++ src/core/util/Pair.ts
@@ -0,0 +1,15 @@
+﻿export class Pair<F, S> {
+
+  first: F;
+  second: S;
+
+  /**
+   * A class which allows you to store two items separately
+   * @param first First item
+   * @param second Second item
+   */
+  constructor(first: F, second: S) {
+    this.first = first;
+    this.second = second;
+  }
+}
