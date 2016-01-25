21da9c3ea7944890920f5a99a556d2e89db7c451
Paul Thiel <lordnoname@t-online.de>
2016-01-25 20:18:20 +0100

    feat(): add substitution interface and substitutiontype enum
    Closes #12

---
+++ src/core/enums/timetable/SubstitutionType.ts
@@ -0,0 +1,12 @@
+﻿/**
+ * Every Substitution has an SubstitutionType which defines the Substitution better
+ */
+export enum SubstitutionType {
+  
+  SUBSTITUTION,
+  ROOMCHANGE,
+  LESSONMERGE,
+  LESSONSPLIT,
+  CANCELEDLESSON
+
+}
---
+++ src/core/interfaces/Substitution.ts
@@ -0,0 +1,33 @@
+﻿import {LessonTime} from '../enums/timetable/LessonTime';
+import {Form} from './Form';
+import {Teacher} from './Teacher';
+import {Room} from './Room';
+import {SubstitutionType} from '../enums/SubstitutionType';
+
+export class Substitution {
+  
+  lessonTime: LessonTime;
+  form: Form;
+  oldTeacher: Teacher;
+  oldRoom: Room;
+  newTeacher: Teacher;
+  newRoom: Room;
+  type: SubstitutionType;
+  date: Date;
+
+  /**
+   * The Substitution identifies the substitute of the absent teacher
+   * @param date Date when the substitution takes place
+   * @param time Time in which lesson it happenes
+   * @param form Form which is affected
+   * @param oldTeacher Teacher who is absent
+   * @param oldRoom Room where the lesson is reguraly
+   */
+  constructor(date: Date, lessonTime: LessonTime, form: Form, oldTeacher: Teacher, oldRoom: Room) {
+    this.lessonTime = lessonTime;
+    this.form = form;
+    this.oldTeacher = oldTeacher;
+    this.oldRoom = oldRoom;
+    this.date = date;
+  }
+}
