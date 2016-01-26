ea25032010f3d4f27caefe3932837f833703d8c9
Paul Thiel <lordnoname@t-online.de>
2016-01-26 19:40:20 +0100

    feat(): begin angularapp
    Closes #19 Fixes #16

---
+++ www/scripts/app.js
@@ -0,0 +1,13 @@
+﻿var Config = require('../Config');
+var app = angular.module('wingo2', ['ngMaterial']);
+
+app.config(function ($mdThemingProvider) {
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('orange')
+    .accentPalette('gray');
+});
+
+app.controller('AppCtrl', function ($scope) {
+  $scope.Title = Config.windowTitle;
+});
