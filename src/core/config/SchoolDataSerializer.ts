2933bf04f5ce68e45ef540779643dd8567a1e2cf
Paul Gschwendtner <paulgschwendtner@me.com>
2016-01-30 15:30:19 +0100

    fix(): convert read buffer into string to be able to getting parsed.
    References #33

--- src/core/config/SchoolDataSerializer.ts
+++ src/core/config/SchoolDataSerializer.ts
@@ -1,4 +1,4 @@
-﻿import fs = require('fs');
+﻿import * as fs from 'fs';
 import { SchoolData } from '../objects/SchoolData';
 
 export class SchoolDataSerializer {
@@ -31,19 +31,23 @@ export class SchoolDataSerializer {
    * Reads the school data from JSON files and stores them
    */
   readData(): void {
-    for (var form in JSON.parse(fs.readFileSync(this.path + 'forms.json'))) {
+    for (var form in JSON.parse(fs.readFileSync(this.path + 'forms.json').toString())) {
       this.schoolData.addForm(form);
     }
-    for (var formLevel in JSON.parse(fs.readFileSync(this.path + 'formLevels.json'))) {
+
+    for (var formLevel in JSON.parse(fs.readFileSync(this.path + 'formLevels.json').toString())) {
       this.schoolData.addFormLevel(formLevel);
     }
-    for (var room in JSON.parse(fs.readFileSync(this.path + 'rooms.json'))) {
+
+    for (var room in JSON.parse(fs.readFileSync(this.path + 'rooms.json').toString())) {
       this.schoolData.addRoom(room);
     }
-    for (var subject in JSON.parse(fs.readFileSync(this.path + 'subjects.json'))) {
+
+    for (var subject in JSON.parse(fs.readFileSync(this.path + 'subjects.json').toString())) {
       this.schoolData.addSubject(subject);
     }
-    for (var teacher in JSON.parse(fs.readFileSync(this.path + 'teachers.json'))) {
+
+    for (var teacher in JSON.parse(fs.readFileSync(this.path + 'teachers.json').toString())) {
       this.schoolData.addTeacher(teacher);
     }
   }
